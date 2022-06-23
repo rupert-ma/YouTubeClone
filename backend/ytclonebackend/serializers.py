ca82515cc931e7328bb6a2d456ea835bcd476eb2
rupert-ma <michael.a.rupert@gmail.com>
2022-06-23 11:38:36 -0700

    created comment and reply serializers

---
+++ backend/ytclonebackend/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Comment, Reply
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['video_id', 'text', 'likes', 'dislikes', 'user_id']
+
+class ReplySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reply
+        fields = ['id', 'comment', 'text', 'user_id']
